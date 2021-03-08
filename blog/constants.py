799166e854694ac786bcf09eb5fe8ad8c1696394
Daniel Reyes Veas <danielreyesveas@gmail.com>
2021-03-08 19:29:13 +0100

    EMAIL_SENDER_URL

--- blog/constants.py
+++ blog/constants.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 contact_template_slug =  'cuenteros_contact'
 newsletter_template_slug =  'cuenteros_newsletter'
-email_sender_url = settings.EMAIL_SENDER_API
+email_sender_url = settings.EMAIL_SENDER_URL
